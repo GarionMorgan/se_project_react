12b4807d3b9b1e76fd6bb59708ac17af9570d80b
github.com/GarionMorgan <garmorgan14@yahoo.com>
2025-09-20 15:51:44 -0500

    ModalWithForm opening and closing update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,18 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.svg";
 
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="What To Wear Logo" />
       <p className="header__date-and-location">Date, Location</p>
-      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <button
+        className="header__add-clothes-btn"
+        type="button"
+        onClick={handleAddClick}
+      >
+        + Add clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
